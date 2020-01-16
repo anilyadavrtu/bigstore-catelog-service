bc66e09a555d0848f5da439b971d8e61f41e7b9b
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-16 19:47:33 +0530

    base project setup

---
+++ src/main/java/com/bigstore/bigstorecatelogservice/BigstoreCatelogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstorecatelogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigstoreCatelogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigstoreCatelogServiceApplication.class, args);
+	}
+
+}
